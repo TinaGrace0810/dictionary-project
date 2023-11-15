ebdf101d73bf6a52c1954ed1d41cc69a44d401cc
TinaGrace0810 <130494896+TinaGrace0810@users.noreply.github.com>
2023-11-15 17:54:12 +0000

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,5 +2,15 @@ import React from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-  return <div className="Dictionary">"Hello from Dictionary"</div>;
+  function search() {
+    alert("Searching");
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} />
+      </form>
+    </div>
+  );
 }
