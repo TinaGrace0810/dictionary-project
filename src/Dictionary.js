0c5fabbce0217467f0f6a9e2653613c4f5ae0407
TinaGrace0810 <130494896+TinaGrace0810@users.noreply.github.com>
2023-11-15 18:00:42 +0000

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
   function search(event) {
     event.preventDefault();
     alert("Searching");
   }
 
+  function handleKeywordChange(event) {
+    console.log(event);
+  }
+
   return (
     <div className="Dictionary">
       <form onSubmit={search}>
-        <input type="search" autoFocus={true} />
+        <input type="search" onChange={handleKeywordChange} />
       </form>
     </div>
   );
