45478595a262f339faf68e5384b0a8dd90ac016f
TinaGrace0810 <130494896+TinaGrace0810@users.noreply.github.com>
2023-11-16 17:58:01 +0000

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl =
+      "https://api.shecodes.io/dictionary/v1/define?word={word}&key={key}";
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
