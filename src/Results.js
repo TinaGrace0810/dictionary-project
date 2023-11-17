1f1b6a5c6d8b378b44f936afcb1b6e77faa4f893
TinaGrace0810 <130494896+TinaGrace0810@users.noreply.github.com>
2023-11-17 20:27:33 +0000

    meaning

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,22 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
-  console.log(props.results);
-  return <div className="Results">Hello from Results</div>;
+    if (props.results) {
+        return (
+            <div className="Results">
+                <h2>{props.results.word}</h2>
+                {props.results.meaning.map(function)(meaning, index) {
+                return (
+                <div key={index}>
+                    <Meaning meaning={meaning} />
+                </div>
+                )
+                return meaning.definitions[0]
+                )}}
+            </div>
+        );
+    } else {
+            return null;
+        }
 }
