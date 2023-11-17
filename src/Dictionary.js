29d0f3a89358d7b97b793b19297aec226a7bcf2d
TinaGrace0810 <130494896+TinaGrace0810@users.noreply.github.com>
2023-11-17 20:54:47 +0000

    week 1

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,52 +1,34 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
-import Results from "./Result";
 
-export default function Dictionary(props) {
-  let [keyword, setKeyword] = useState(props.defaultKeyword);
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
-  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    setResults(response.data);
+    setResults(response.data[0]);
   }
 
-  function search() {
-    //documentation:https://www.shecodes.io/learn/apis/dictionary
-
-    let apiKey = "eaat2082d1dabe69502o3fa4aa574ecb";
-    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
-  }
-
-  function handleSubmit(event) {
+  function search(event) {
     event.preventDefault();
-    search();
+
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-  function load() {
-    setLoaded(true);
-    search();
-  }
-  if (loaded) {
-    return (
-      <div className="Dictionary">
-        <section>
-          <h1>What word do you want to look up?</h1>
-          <form onSubmit={handleSubmit}>
-            <input type="search" onChange={handleKeywordChange} />
-          </form>
-          <div className="hint">suggested words: sunset, yoga, plant..</div>
-        </section>
-        <Results results={results} />
-      </div>
-    );
-  } else {
-    load();
-    return "loading..";
-  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+      <Results results={results} />
+    </div>
+  );
 }
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,18 +1,20 @@
 import React from "react";
-import Synonyms from "./Synonyms";
-import "./Meaning.css";
 
 export default function Meaning(props) {
-  if (props.meaning) {
-    return (
-      <div className="Meaning">
-        <h3>{props.meaning.partOfSpeech}</h3>
-        <p>{props.meaning.definition}</p>
-        <em>{props.meaning.example}</em>
-        <Synonyms synonyms={props.meaning.synonyms} />
-      </div>
-    );
-  } else {
-    return null;
-  }
+  return (
+    <div className="Meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
