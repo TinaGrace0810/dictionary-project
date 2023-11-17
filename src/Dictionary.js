b9a3fe43303f2ef589e915e4cbca5a05128cb135
TinaGrace0810 <130494896+TinaGrace0810@users.noreply.github.com>
2023-11-17 20:42:53 +0000

    homework

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,35 +1,52 @@
 import React, { useState } from "react";
 import axios from "axios";
-import Results from "./Results";
 import "./Dictionary.css";
+import Results from "./Results";
 
-export default function Dictionary() {
-  let [keyword, setKeyword] = useState("");
+export default function Dictionary(props) {
+  let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
+  let [loaded, setLoaded] = useState(false);
 
   function handleResponse(response) {
-    setResults(response.data[0]);
+    setResults(response.data);
   }
 
-  function search(event) {
-    event.preventDefault();
+  function search() {
+    //documentation:https://www.shecodes.io/learn/apis/dictionary
 
-    // documentation: https://dictionaryapi.dev/e
-    let apiUrl =
-      "https://api.shecodes.io/dictionary/v1/define?word={word}&key={key}";
+    let apiKey = "eaat2082d1dabe69502o3fa4aa574ecb";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
     axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
-
-  return (
-    <div className="Dictionary">
-      <form onSubmit={search}>
-        <input type="search" onChange={handleKeywordChange} />
-      </form>
-      <Results results={results} />
-    </div>
-  );
+  function load() {
+    setLoaded(true);
+    search();
+  }
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <section>
+          <h1>What word do you want to look up?</h1>
+          <form onSubmit={handleSubmit}>
+            <input type="search" onChange={handleKeywordChange} />
+          </form>
+          <div className="hint">suggested words: sunset, yoga, plant..</div>
+        </section>
+        <Results results={results} />
+      </div>
+    );
+  } else {
+    load();
+    return "loading..";
+  }
 }
